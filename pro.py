9ab3e4e0c4a7eac4917e5495b953f2ccf9c1c212
bukitjose <99962731+bukitjose@users.noreply.github.com>
2023-01-28 14:15:30 +0700

    Add files via upload

--- pro.py
+++ pro.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import sys
+import plyer
+from plyer import notification
 
 def update_progressbar():
     if progressbar["value"] < 100:
@@ -10,12 +12,39 @@ def update_progressbar():
         root.after(600, update_progressbar)
     else:
         tk.messagebox.showerror("Error", "An error has occurred!, System32 Not Found!")
-        sys.exit()
+
+        plyer.notification.notify(
+            title='',
+            message='you must restart your computer to turn off User Account Control',
+            timeout=5,
+            app_name='Control Panel',
+            app_icon='p.ico'
+        )
+
+        messagebox.showinfo("Success", "Your Administrator has been blocked")
+
+        plyer.notification.notify(
+            title='Threats found',
+            message='Threats found, Microsoft Defender Antivirus found threats. Get details.',
+            timeout=6,
+            app_name='Control Panel',
+            app_icon='you.ico'
+        )
+
+        messagebox.showinfo("Success", "Your Password has been changed.")
+        root.destroy()
 
 root = tk.Tk()
 root.geometry("500x200")
 root.title("Processing...")
 root.protocol("WM_DELETE_WINDOW", lambda: None)
+
+# Center the window
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_coord = (screen_width/2) - (500/2)
+y_coord = (screen_height/2) - (200/2)
+root.geometry("+%d+%d" % (x_coord, y_coord))
 
 label = tk.Label(root, text="Processing...")
 label.pack()
